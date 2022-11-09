067005e82b3dc252d63ba2dd5fa7c37c100abaf8
Shailesh-engg <117379943+Shailesh-engg@users.noreply.github.com>
2022-11-09 14:36:43 +0530

    practice 09/11

--- practice-01.py
+++ practice-01.py
@@ -1,3 +1,17 @@
-name="shailesh"
-f_name="kulal"
-print(name+" "+f_name)
+a=input("hello you want to play game ?")
+if a == "yes":
+  print("wellcome lets start")
+elif a== "no":
+  print("okey fyne")
+else:
+  print("its okey")
+
+num = int(input("enter a number : "))
+if num > 100 :
+  print("sorry enter between 100: ")
+elif num == 100 :
+  print("yaar enter some other number: ")
+else:
+  print("you entered correct number")
+
+    
